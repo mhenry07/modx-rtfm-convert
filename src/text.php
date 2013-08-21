df48cb600f24f4d0a785a77f50682c04eec16f60
Mike Henry <mhenry07@gmail.com>
2013-08-21 07:58:52 -0700

    Add note about creating diffs

--- src/text.php
+++ src/text.php
@@ -3,6 +3,8 @@ require '../vendor/autoload.php';
 
 // should probably use indent => false and wrap => 0 when doing text comparisons
 // TODO: trim groups of empty lines to a single empty line
+// could use xdiff for creating diffs http://www.php.net/manual/en/book.xdiff.php
+// or maybe git diff (--numstat and --dirstat options might be helpful)
 
 $src = '../data/tidy.html';
 $dest = '../data/tidy.txt';

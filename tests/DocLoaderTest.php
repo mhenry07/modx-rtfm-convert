fa5e9601ca806037f806f22e57604f2456c80d8f
Mike Henry <mhenry07@gmail.com>
2013-08-24 01:39:46 -0700

    Set up autoloading for namespaces referenced by tests

--- tests/DocLoaderTest.php
+++ tests/DocLoaderTest.php
@@ -1,16 +1,11 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
  * User: mhenry
  * Date: 8/23/13
  * Time: 6:53 PM
- * To change this template use File | Settings | File Templates.
  */
 
 namespace RtfmConvert;
-
-require_once '../src/DocLoader.php';
-
 
 class DocLoaderTest extends \PHPUnit_Framework_TestCase {
 

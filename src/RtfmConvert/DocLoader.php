b2ed2e494630b834cd04a741fdf812f6e4f31206
Mike Henry <mhenry07@gmail.com>
2013-08-24 02:59:04 -0700

    Add the ability to get local files

--- src/RtfmConvert/DocLoader.php
+++ src/RtfmConvert/DocLoader.php
@@ -6,12 +6,17 @@ class DocLoader {
 
     /**
      * @param string $url The URL of the web page to get.
+     * @param string $filename A local copy of the page to try loading first.
      * @throws RtfmException
      * @return string Returns the contents of the response.
      */
-    public function get($url) {
+    public function get($url, $filename = null) {
+        $source = $url;
+        if (!is_null($filename) && file_exists($filename))
+            $source = $filename;
+
         try {
-            return file_get_contents($url);
+            return file_get_contents($source);
         } catch (\Exception $e) {
             throw new RtfmException($e->getMessage(), $e->getCode(), $e);
         }
--- tests/DocLoaderTest.php
+++ tests/DocLoaderTest.php
@@ -8,19 +8,50 @@
 namespace RtfmConvert;
 
 class DocLoaderTest extends \PHPUnit_Framework_TestCase {
+    const DATA_DIR = '../data/test/';
+
+    /** @var DocLoader */
+    private $docLoader;
+
+    private $tempFile;
+
+    public function setUp() {
+        $this->docLoader = new DocLoader();
+        if (!file_exists(self::DATA_DIR))
+            mkdir(self::DATA_DIR, 0777, true);
+    }
+
+    public function tearDown() {
+        if (isset($this->tempFile) && file_exists($this->tempFile))
+            unlink($this->tempFile);
+    }
 
     public function testGetShouldRetrieveWebPage() {
-        $docLoader = new DocLoader();
-
-        $page = $docLoader->get('http://www.google.com/');
+        $page = $this->docLoader->get('http://www.google.com/');
         $this->assertInternalType('string', $page);
         $this->assertContains('<html', $page);
     }
 
     public function testGetShouldThrowRtfmExceptionOnError() {
-        $docLoader = new DocLoader();
-
         $this->setExpectedException('\RtfmConvert\RtfmException');
-        $docLoader->get('http://localhost/invalid-url');
+        $this->docLoader->get('http://localhost/invalid-url');
+    }
+
+    public function testGetShouldLoadLocalFile() {
+        $this->tempFile = self::DATA_DIR . 'DocLoader.get';
+        $expectedContents = 'local';
+        file_put_contents($this->tempFile, $expectedContents);
+
+        $page = $this->docLoader->get('http://www.google.com/', $this->tempFile);
+        $this->assertStringEqualsFile($this->tempFile, $page);
+    }
+
+    public function testGetShouldLoadRemotePageWhenFileNotFound() {
+        $filename = 'non-existent-file';
+        if (file_exists($filename))
+            $this->markTestSkipped("This test requires that the file '{$filename}' not exist.");
+
+        $page = $this->docLoader->get('http://www.google.com/', $filename);
+        $this->assertContains('<html', $page);
     }
 }

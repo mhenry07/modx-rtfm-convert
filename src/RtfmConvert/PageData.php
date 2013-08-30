92ef75f96d2a48275071d597420e134fe4afb663
Mike Henry <mhenry07@gmail.com>
2013-08-29 18:20:45 -0700

    Implement PageData

---
+++ src/RtfmConvert/PageData.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @author: Mike Henry
+ */
+
+namespace RtfmConvert;
+
+
+class PageData {
+    /** @var \QueryPath\DOMQuery|string */
+    protected $html;
+
+    /** @var PageStatistics */
+    protected $stats;
+
+    /**
+     * @param \QueryPath\DOMQuery|string $html
+     * @param PageStatistics $stats
+     */
+    public function __construct($html, PageStatistics $stats = null) {
+        $this->html = $html;
+        $this->stats = $stats;
+    }
+
+    /**
+     * @todo allow getting full document (inc. doctype)
+     * Note: if there are multiple matches, assumes they are contiguous
+     * @return string Returns the HTML as a string. If the internal type
+     * is a \QueryPath\DOMQuery, converts it to a string starting with the
+     * current selector.
+     */
+    public function getHtmlString() {
+        if (is_string($this->html))
+            return $this->html;
+        $result = '';
+        /** @var \QueryPath\DOMQuery $item */
+        foreach ($this->html as $item)
+            $result .= $this->html->document()->saveHTML($item->get(0));
+        return $result;
+    }
+
+    /**
+     * @return \QueryPath\DOMQuery
+     */
+    public function getHtmlQuery() {
+        if (is_string($this->html))
+            return htmlqp($this->html);
+        return $this->html;
+    }
+
+    /**
+     * @return PageStatistics
+     */
+    public function getStats() {
+        return $this->stats;
+    }
+}
---
+++ tests/unit/RtfmConvert/PageDataTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author: Mike Henry
+ */
+
+namespace RtfmConvert;
+
+
+class PageDataTest extends HtmlTestCase {
+
+    public function testGetHtmlStringShouldReturnGivenHtmlString() {
+        $html = '<html><head><title>Test</title></head><body></body></html>';
+        $pageData = new PageData($html);
+        $this->assertEquals($html, $pageData->getHtmlString());
+    }
+
+    public function testGetHtmlStringShouldReturnExpectedStringGivenDomQuery() {
+        $expected = <<<'EOT'
+<html>
+<head><title>Test</title></head>
+<body></body>
+</html>
+EOT;
+
+        $qp = htmlqp('<html><head><title>Test</title></head><body></body></html>');
+        $pageData = new PageData($qp);
+        $this->assertEquals($expected, $pageData->getHtmlString());
+    }
+
+    public function testGetHtmlQueryShouldReturnGivenDomQuery() {
+        $qp = htmlqp('<html><head><title>Test</title></head><body></body></html>');
+        $pageData = new PageData($qp);
+        $this->assertEquals($qp, $pageData->getHtmlQuery());
+    }
+
+    public function testGetHtmlQueryShouldReturnExpectedDomQueryGivenHtmlString() {
+        $html = '<html><head><title>Test</title></head><body></body></html>';
+        $qp = htmlqp($html);
+        $pageData = new PageData($html);
+        $this->assertHtmlEquals($qp, $pageData->getHtmlQuery());
+    }
+}

284e0c7b137e5f28fc19b103a2eb33d812b0ba73
Mike Henry <mhenry07@gmail.com>
2013-09-09 14:27:42 -0700

    Add assertValueStat and assertTransformStat
    Also deprecated assertStat.

--- tests/unit/RtfmConvert/HtmlTestCase.php
+++ tests/unit/RtfmConvert/HtmlTestCase.php
@@ -56,6 +56,9 @@ class HtmlTestCase extends \PHPUnit_Framework_TestCase {
             $this->normalizeHtml($actualQp), $message);
     }
 
+    /**
+     * @deprecated
+     */
     public function assertStat($expectedLabel, $expectedValue, $expectedTransformed = null, $expectedWarning = null) {
         if (is_null($this->stats))
             $this->fail('the test class requires a valid PageStatistics object');
@@ -67,6 +70,54 @@ class HtmlTestCase extends \PHPUnit_Framework_TestCase {
             $this->assertEquals($expectedTransformed, $stat['transformed']);
         if (!is_null($expectedWarning))
             $this->assertEquals($expectedWarning, $stat['warning']);
+    }
+
+    /*
+     * @param string $label
+     * @param int $expectedFound
+     * @param array $options An associative array of options.
+     *  Possible options:
+     * * warnings: an int representing the number of warnings
+     */
+    public function assertValueStat($expectedLabel, $expectedValue,
+                                    array $options = array()) {
+//                                    $expectedWarnings = null) {
+        if (is_null($this->stats))
+            $this->fail('the test class requires a valid PageStatistics object');
+        $statsArray = $this->stats->getStats();
+        $this->assertArrayHasKey($expectedLabel, $statsArray);
+        $stat = $statsArray[$expectedLabel];
+        $this->assertEquals($expectedValue, $stat[PageStatistics::VALUE]);
+
+        $expectedWarnings = $this->getOption($options, 'warnings');
+        if (!is_null($expectedWarnings))
+            $this->assertEquals($expectedWarnings, $stat[PageStatistics::WARNING]);
+    }
+
+
+    /*
+     * @param string $label
+     * @param int $expectedFound
+     * @param array $options An associative array of options.
+     *  Possible options:
+     * * transformed: an int representing the number of transformations performed
+     * * warnings: an int representing the number of warnings
+     */
+    public function assertTransformStat($label, $expectedFound,
+                                        array $options = array()) {
+        if (is_null($this->stats))
+            $this->fail('the test class requires a valid PageStatistics object');
+        $statsArray = $this->stats->getStats();
+        $this->assertArrayHasKey($label, $statsArray);
+        $stat = $statsArray[$label];
+        $this->assertEquals($expectedFound, $stat[PageStatistics::FOUND]);
+
+        $expectedTransformed = $this->getOption($options, 'transformed');
+        if (!is_null($expectedTransformed))
+            $this->assertEquals($expectedTransformed, $stat[PageStatistics::TRANSFORM]);
+        $expectedWarnings = $this->getOption($options, 'warnings');
+        if (!is_null($expectedWarnings))
+            $this->assertEquals($expectedWarnings, $stat[PageStatistics::WARNING]);
     }
 
     protected function normalizeHtml(DOMQuery $qp) {
@@ -86,5 +137,9 @@ class HtmlTestCase extends \PHPUnit_Framework_TestCase {
             'tidy-mark' => false);
         $tidy = new tidy();
         return $tidy->repairString($html, $config, 'utf8');
+    }
+
+    protected function getOption(array $options, $key) {
+        return array_key_exists($key, $options) ? $options[$key] : null;
     }
 }

de5eeab085f6ab93167de72d9e1b1f8ee96c4664
Mike Henry <mhenry07@gmail.com>
2013-09-10 13:18:00 -0700

    Add AbstractHtmlTransformer::executeTransformStep method

--- src/RtfmConvert/HtmlTransformers/AbstractHtmlTransformer.php
+++ src/RtfmConvert/HtmlTransformers/AbstractHtmlTransformer.php
@@ -6,6 +6,7 @@
 namespace RtfmConvert\HtmlTransformers;
 
 
+use QueryPath\DOMQuery;
 use RtfmConvert\PageData;
 use RtfmConvert\PageStatistics;
 use RtfmConvert\ProcessorOperationInterface;
@@ -29,5 +30,41 @@ abstract class AbstractHtmlTransformer implements ProcessorOperationInterface {
     public function process($pageData) {
         $qp = $this->transform($pageData);
         return new PageData($qp, $pageData->getStats());
+    }
+
+    /**
+     * Executes a transform step including adding a stat, executing the
+     * transformation and checking the number of elements added or removed by
+     * the operation.
+     *
+     * @param string $label A unique label for the stat.
+     * @param DOMQuery $query The QueryPath object that $addStatFn will base
+     * its stat on and that $transformFn will modify.
+     * @param PageData $pageData Contains the PageStatistics to be updated.
+     * @param callable $transformFn Callback to execute the transformation.
+     * Arguments: DOMQuery $query
+     * @param callable $addStatFn Callback to add a stat.
+     * Arguments: string $label, DOMQuery $query, PageData $pageData
+     * @param int|callable $getExpectedElementDiff An expected count or a
+     * callback to calculate the expected count. Arguments: DOMQuery $query
+     */
+    protected function executeTransformStep($label, DOMQuery $query,
+                                            PageData $pageData,
+                                            callable $transformFn,
+                                            callable $addStatFn,
+                                            $getExpectedElementDiff) {
+        $pageData->beginTransform($query);
+
+        $expectedElementDiff = 0;
+        if (is_integer($getExpectedElementDiff))
+            $expectedElementDiff = $getExpectedElementDiff;
+        if (is_callable($getExpectedElementDiff))
+            $expectedElementDiff = $getExpectedElementDiff($query);
+
+        $addStatFn($label, $query, $pageData);
+
+        $transformFn($query);
+
+        $pageData->checkTransform($label, $query, $expectedElementDiff);
     }
 }

36e24e3112b4b146fdffcd9b17f64a64e30bee55
Mike Henry <mhenry07@gmail.com>
2013-09-11 22:16:37 -0700

    Add transform messages to ConfluenceAsideHtmlTransformer

--- src/RtfmConvert/HtmlTransformers/ConfluenceAsideHtmlTransformer.php
+++ src/RtfmConvert/HtmlTransformers/ConfluenceAsideHtmlTransformer.php
@@ -24,15 +24,16 @@ class ConfluenceAsideHtmlTransformer extends AbstractHtmlTransformer {
         // I think it's -8 per .panelMacro
         $expectedDiff = -(RtfmQueryPath::countAll($asides) -
             RtfmQueryPath::countAll($asides->find('table > tr > td:nth-child(2)')));
-        $pageData->addQueryStat('asides', $asides,
-            array(self::TRANSFORM_ALL => true));
+        $pageData->addQueryStat('asides', $asides);
 
         $pageData->beginTransform($qp);
-        $asides->each(function ($index, $item) {
+        $asides->each(function ($index, $item) use ($pageData) {
             $panel = qp($item);
             $table = $panel->find('table')->first();
             foreach ($this->types as $type) {
                 if ($table->attr('class') == "{$type}Macro") {
+                    $pageData->incrementStat('asides', self::TRANSFORM, 1,
+                        "extracting content to div.{$type}");
                     $panel->removeClass('panelMacro')->addClass($type);
                     $table->remove();
                     $table->find('td')->eq(1)->contents()

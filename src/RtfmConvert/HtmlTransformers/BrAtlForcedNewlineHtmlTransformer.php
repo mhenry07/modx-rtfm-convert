90431660f809800f1a936372850337606fb2a0fa
Mike Henry <mhenry07@gmail.com>
2013-09-13 00:51:20 -0700

    Change DOMQuery::is() usage in BrAtlForcedNewlineHtmlTransformer
    QueryPath is() seems to give false positives in some cases when a
    string is supplied, so I changed it to check against a DOMNode instead.

--- src/RtfmConvert/HtmlTransformers/BrAtlForcedNewlineHtmlTransformer.php
+++ src/RtfmConvert/HtmlTransformers/BrAtlForcedNewlineHtmlTransformer.php
@@ -17,12 +17,12 @@ class BrAtlForcedNewlineHtmlTransformer extends AbstractHtmlTransformer {
         $pageData->beginTransform($qp);
         $expectedDiff = 0;
 
-        if ($qp->firstChild()->is('p > br.atl-forced-newline:only-child')) {
+        if ($this->isFirstElementEmptyPBr($pageData)) {
             $pageData->incrementStat($label, self::TRANSFORM, 1, 'removed first');
             $expectedDiff -= 2;
             $qp->firstChild()->remove();
         }
-        if ($qp->lastChild()->is('p > br.atl-forced-newline:only-child')) {
+        if ($this->isLastElementEmptyPBr($pageData)) {
             $pageData->incrementStat($label, self::TRANSFORM, 1, 'removed last');
             $expectedDiff -= 2;
             $qp->lastChild()->remove();
@@ -37,9 +37,27 @@ class BrAtlForcedNewlineHtmlTransformer extends AbstractHtmlTransformer {
         return $qp;
     }
 
-    // should I add stats for first & last?
     protected function generateStatistics(PageData $pageData) {
         $atlNewlines = $pageData->getHtmlQuery('br.atl-forced-newline');
         $pageData->addQueryStat('br.atl-forced-newline', $atlNewlines);
+    }
+
+    // QueryPath is() seems to return true in some cases when a descendant
+    // matches the selector when it's a string. It should only match if the
+    // element itself matches the selector.
+    protected function isFirstElementEmptyPBr(PageData $pageData) {
+        $qp = $pageData->getHtmlQuery();
+        $firstBr = $qp->find('p > br.atl-forced-newline:only-child')->first();
+        if ($firstBr->count() == 0)
+            return false;
+        return $qp->firstChild()->is($firstBr->parent()->get(0));
+    }
+
+    protected function isLastElementEmptyPBr(PageData $pageData) {
+        $qp = $pageData->getHtmlQuery();
+        $lastBr = $qp->find('p > br.atl-forced-newline:only-child')->last();
+        if ($lastBr->count() == 0)
+            return false;
+        return $qp->lastChild()->is($lastBr->parent()->get(0));
     }
 }

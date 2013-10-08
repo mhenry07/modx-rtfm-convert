1721f3bfdaeae90f523f11f1781960e8481e2a39
Mike Henry <mhenry07@gmail.com>
2013-10-08 16:10:59 -0700

    Change DocOrganizer to organize all docs in imported containers

--- src/import.php
+++ src/import.php
@@ -4,6 +4,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use RtfmImport\ContentFixer;
 use RtfmImport\DocImporter;
 use RtfmImport\DocOrganizer;
+use RtfmImport\ResourceGroupSetter;
 
 $config = include dirname(__FILE__) . '/import.config.php';
 include $config['modx_core_config'];
@@ -15,9 +16,9 @@ $modx->setLogLevel(modX::LOG_LEVEL_INFO);
 $modx->setLogTarget('ECHO');
 
 $importer = new DocImporter($config, $modx);
-$organizer = new DocOrganizer($modx);
+$organizer = new DocOrganizer($config, $modx);
 $fixer = new ContentFixer($config, $modx);
-$groupSetter = new \RtfmImport\ResourceGroupSetter($modx);
+$groupSetter = new ResourceGroupSetter($modx);
 
 $imported = $importer->import();
 $imported = $organizer->organize($imported);

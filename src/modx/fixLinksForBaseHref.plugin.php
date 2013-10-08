a0760c24626dd64ccd05f9426258c44392c44ad2
Mike Henry <mhenry07@gmail.com>
2013-10-08 12:17:36 -0700

    Make improvements to fixLinksForBaseHref plugin

--- src/modx/fixLinksForBaseHref.plugin.php
+++ src/modx/fixLinksForBaseHref.plugin.php
@@ -5,16 +5,10 @@
 
 $resource = $modx->resource;
 $output = &$resource->_output;
-$output = str_replace(
-    array(
-        'href="/',
-        'src="/',
-        'href="#',
-    ),
-    array(
-        'href="',
-        'src="',
-        "href=\"{$resource->get('context_key')}/{$resource->get('uri')}#",
-    ),
-    $output
-);
+
+// remove root slash from relative url in href or src attribute
+$output = preg_replace('#(<\w+\b[^>]+\b(?:href|src)=[\'"]?)/(?!>)#i', '$1',
+    $output);
+// prepend anchor with page url
+$output = preg_replace('/(<(?:a|area)\b[^>]+\bhref=[\'"]?)#/i',
+    "$1{$resource->get('context_key')}/{$resource->get('uri')}#", $output);
